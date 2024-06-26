44b209700e5f13a76b688ee8dac5638454c0985d
Harsh Kumar Singh <harsh.kumar.official213@gmail.com>
2024-06-27 00:59:52 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
-
+import path from 'path'
 import {app, server} from './socket/socket.js'
 import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.routes.js"
@@ -10,9 +10,10 @@ import userRoutes from "./routes/user.routes.js"
 import { connectDB } from "./db/connectToDB.js";
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 dotenv.config();
 
+const __dirname = path.resolve();
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
@@ -21,13 +22,16 @@ app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
 
-app.get('/',(req,res)=>{
-    // root route https://localhost:5000/
-    
-    res.send("hello world!")
+app.use(express.static(path.join(__dirname,'/frontend/dist')))
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
 })
-
-
+// app.get('/',(req,res)=>{
+//     // root route https://localhost:5000/
+    
+//     res.send("hello world!")
+// })
 
 
 server.listen(PORT,()=>{
